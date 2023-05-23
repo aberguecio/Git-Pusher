76540afdbb8831cb7599ecb5a90d4ca8ceb14825
aberguecio <aberguecio1@uc.cl>
2023-05-23 07:36:24 +0000

    Automatic commit

--- GitPusher/settings.py
+++ GitPusher/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
 ]
